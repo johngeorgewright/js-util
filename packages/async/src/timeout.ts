5ba6de0786c43929a12537c787fa69bbee90c54b
John Wright <jrite@protonmail.com>
2021-06-23 16:32:06 +0100

    fix: always use abort polyfill

--- packages/async/src/timeout.ts
+++ packages/async/src/timeout.ts
@@ -1,4 +1,5 @@
 import AbortError from './AbortError'
+import { AbortSignal } from 'node-abort-controller'
 
 export default async function timeout(ms: number = 0, signal?: AbortSignal) {
   return new Promise<void>((resolve, reject) => {
