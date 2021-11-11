1dae69b5c18cc95465e5085a8dd5a08edcdf0110
John Wright <jrite@protonmail.com>
2021-11-11 14:50:56 +0000

    perf(iterator): simplify sorting composition

--- packages/iterator/src/sort.ts
+++ packages/iterator/src/sort.ts
@@ -4,10 +4,11 @@ export default function sort<T>(
   maxChunkSize: number,
   fn?: (a: T, b: T) => number
 ) {
+  const _take = take<T>(maxChunkSize)
   return function* _sort(
     generator: Generator<T>
   ): Generator<T, any, undefined> {
-    const collection = [...take<T>(maxChunkSize)(generator)]
+    const collection = [..._take(generator)]
     if (!collection.length) return
     yield* collection.sort(fn)
     yield* _sort(generator)
