21f9b63856b009ac7d223905cbca5a286bddca32
John Wright <jrite@protonmail.com>
2021-11-11 14:04:24 +0000

    feat(iterator): add map

--- packages/iterator/src/index.ts
+++ packages/iterator/src/index.ts
@@ -1,1 +1,2 @@
 export { default as IteratorHandler } from './IteratorHandler'
+export { default as map } from './map'
---
+++ packages/iterator/src/map.ts
@@ -0,0 +1,7 @@
+export default function map<In, Out>(fn: (i: In) => Out) {
+  return function* (generator: Generator<In>) {
+    for (const item of generator) {
+      yield fn(item)
+    }
+  }
+}
