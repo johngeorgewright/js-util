19b5370bf8ea70b165e2c5d9db1a805a8e15ca2d
John Wright <jrite@protonmail.com>
2021-11-30 07:23:52 +0000

    fix(object): map typing

--- packages/object/src/map.ts
+++ packages/object/src/map.ts
@@ -1,12 +1,9 @@
-export default function map<A, B>(
-  a: Record<string, A>,
-  fn: (value: A, key: string) => B
-) {
-  const b: Record<string, B> = {}
-
-  for (const key in a) {
-    b[key] = fn(a[key], key)
-  }
-
-  return b
+export default function map<
+  I extends Record<string | number | symbol, unknown>,
+  O extends { [K in keyof I]: unknown }
+>(i: I, fn: <K extends keyof I>(value: I[K], key: K) => O[K]) {
+  return Object.entries(i).reduce(
+    (o, [k, v]) => ({ ...o, [k]: fn(v as I[keyof I], k) }),
+    {} as O
+  )
 }
