cb357cdf63678efcbe9a38fad9b5289540b8b679
John Wright <jrite@protonmail.com>
2021-12-17 15:06:43 +0000

    test: relative paths only
    Absolute paths will be different on all machines

--- packages/fs/src/__tests__/readDir.test.ts
+++ packages/fs/src/__tests__/readDir.test.ts
@@ -2,14 +2,17 @@ import { accumulate } from '@johngw/async-iterator'
 import * as pathHelper from 'path'
 import readDir from '../readDir'
 
-const fixturesDirname = pathHelper.join(__dirname, 'fixtures')
+const fixturesDirname = pathHelper.relative(
+  process.cwd(),
+  pathHelper.join(__dirname, 'fixtures')
+)
 
 test('read a directory', async () => {
   await expect(accumulate(readDir(fixturesDirname))).resolves
     .toMatchInlineSnapshot(`
           Array [
-            "/Users/john.wright/Workspace/js-util/packages/fs/src/__tests__/fixtures/a.txt",
-            "/Users/john.wright/Workspace/js-util/packages/fs/src/__tests__/fixtures/deep/b.yaml",
+            "src/__tests__/fixtures/a.txt",
+            "src/__tests__/fixtures/deep/b.yaml",
           ]
         `)
 })
