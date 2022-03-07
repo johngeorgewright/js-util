b2c1fe809d0ae4654b3a7382e39f4d500483c232
John Wright <jrite@protonmail.com>
2022-03-07 14:32:43 +0000

    test: fix tests

--- packages/map/src/remove.test.ts
+++ packages/map/src/remove.test.ts
@@ -3,7 +3,7 @@ import remove from './remove'
 test('removes items by key', () => {
   const map = new Map<number, number>(
     (function* () {
-      for (let i = 0; i < 10; i++) yield [i, i]
+      for (let i = 0; i < 10; i++) yield [i, i] as const
     })()
   )
 
