2646760b95721959dad8b6030903a2d96973aece
John Wright <jrite@protonmail.com>
2022-03-07 16:51:51 +0000

    feat(map): get overloading

--- packages/map/src/get.ts
+++ packages/map/src/get.ts
@@ -1,3 +1,7 @@
+export default function get<K, V>(map: Map<K, V>, key: K): V | undefined
+
+export default function get<K, V>(map: Map<K, V>, key: K, dflt: V): V
+
 export default function get<K, V>(
   map: Map<K, V>,
   key: K,
